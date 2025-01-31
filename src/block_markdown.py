ef463737e28c608f3926daea1d922e5ac9e8d408
Brianstein27 <brianstein27@proton.me>
2025-01-31 20:59:33 +0100

    add block_to_block_type function and unittests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,5 @@
 # handles markdown blocks like heading, lists, paragraphs
+import re
 
 
 def markdown_to_blocks(markdown):
@@ -10,3 +11,44 @@ def markdown_to_blocks(markdown):
             stripped_splits.append(stripped_split)
 
     return stripped_splits
+
+
+def block_to_block_type(block):
+    if block[:3] == "```" and block[-3:] == "```":
+        return "code"
+
+    pattern = r"#{1,6}\s+"
+    match = re.search(pattern, block)
+    if match:
+        return "heading"
+
+    lines = block.split("\n")
+    stripped_lines = []
+    for line in lines:
+        stripped_line = line.strip()
+        if stripped_line != "":
+            stripped_lines.append(stripped_line)
+
+    quotes = map(lambda line: line[0] == ">", stripped_lines)
+    if all(quotes):
+        return "quote"
+
+    unordered_list = map(
+        lambda line: line.strip()[:2] == "* " or line.strip()[:2] == "- ", lines
+    )
+    if all(unordered_list):
+        return "unordered_list"
+
+    index = 0
+    ordered_list = []
+    for line in lines:
+        line = line.strip()
+        if line[0].isnumeric() and int(line[0]) > index and line[1:3] == ". ":
+            ordered_list.append(True)
+        else:
+            ordered_list.append(False)
+
+    if all(ordered_list):
+        return "ordered_list"
+
+    return "paragraph"
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -64,3 +64,83 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_block_to_blocktype_heading1(self):
+        block = "# Hello"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_heading2(self):
+        block = "## Hello"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_heading3(self):
+        block = "### Hello"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_heading4(self):
+        block = "#### Hello"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_heading5(self):
+        block = "##### Hello"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_heading6(self):
+        block = "###### Hello"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_heading7(self):
+        block = "######Hello"
+        blocktype = block_to_block_type(block)
+        self.assertNotEqual(blocktype, "heading")
+
+    def test_block_to_blocktype_code1(self):
+        block = "```hello```"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "code")
+
+    def test_block_to_blocktype_code2(self):
+        block = "```  hello this is a codeblock ```"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "code")
+
+    def test_block_to_blocktype_code3(self):
+        block = "`  hello this is not a codeblock `"
+        blocktype = block_to_block_type(block)
+        self.assertNotEqual(blocktype, "code")
+
+    def test_block_to_blocktype_quote(self):
+        block = """> quote
+        > this is a quote
+        > so is this
+        > and this
+        """
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "quote")
+
+    def test_block_to_block_type_unordered_list1(self):
+        block = """* Hi
+        * Bye
+        * Goodnight"""
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "unordered_list")
+
+    def test_block_to_block_type_unordered_list2(self):
+        block = """- Hi
+        - Bye
+        - Goodnight"""
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "unordered_list")
+
+    def test_block_to_block_type_ordered_list(self):
+        block = """1. Hi
+        2. Bye
+        3. Night"""
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, "ordered_list")
